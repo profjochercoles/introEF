192f817b7aa8191265584379e76060015cd2e743
Andres <orlando.ra.sist@gmail.com>
2022-06-23 17:39:18 -0300

    ABM terminado, resta la vista de cada tabla

--- EFInstituto/Migrations/EFInstitutoContextModelSnapshot.cs
+++ EFInstituto/Migrations/EFInstitutoContextModelSnapshot.cs
@@ -14,13 +14,13 @@ namespace EFInstituto.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
+                .HasAnnotation("ProductVersion", "3.1.25")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.17")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("EFInstituto.Alumno", b =>
                 {
-                    b.Property<int>("id")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
@@ -34,7 +34,7 @@ namespace EFInstituto.Migrations
                     b.Property<int>("ProfesorId")
                         .HasColumnType("int");
 
-                    b.HasKey("id");
+                    b.HasKey("Id");
 
                     b.ToTable("Alumno");
                 });
@@ -62,7 +62,7 @@ namespace EFInstituto.Migrations
 
             modelBuilder.Entity("EFInstituto.Materia", b =>
                 {
-                    b.Property<int>("id")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
@@ -73,30 +73,9 @@ namespace EFInstituto.Migrations
                     b.Property<string>("Descripcion")
                         .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("id");
-
-                    b.ToTable("Materia");
-                });
-
-            modelBuilder.Entity("EFInstituto.Profesor", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<string>("Apellido")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("MateriaId")
-                        .HasColumnType("int");
-
-                    b.Property<string>("Nombre")
-                        .HasColumnType("nvarchar(max)");
-
                     b.HasKey("Id");
 
-                    b.ToTable("Profesor");
+                    b.ToTable("Materia");
                 });
 #pragma warning restore 612, 618
         }
--- EFInstituto/Program.cs
+++ EFInstituto/Program.cs
@@ -5,63 +5,22 @@ using System.Linq;
 
 namespace EFInstituto
 {
-    class Maestro
-    {
-        public int Id { get; set; }
-
-        public string Nombre { get; set; }
-
-        public string Apellido { get; set; }
-
-        public int MateriaId { get; set; }
-    }
-
-    class Alumno
-    {
-        public int id { get; set; }
-
-        public string Nombre { get; set; }
-
-        public string Apellido { get; set; }
-
-        public int ProfesorId { get; set; }
-    }
-
-    class Materia
-    {
-        public int id { get; set; }
-
-        public string Descripcion { get; set; }
-
-        public string CursoDivision { get; set; }
-    }
-
-    class Profesor
-    {
-        public int Id { get; set; }
-
-        public string Nombre { get; set; }
-
-        public string Apellido { get; set; }
-
-        public int MateriaId { get; set; }
-    }
-
     class EFInstitutoContext : DbContext
     {
         public DbSet<Maestro> Maestro { get; set; }
         public DbSet<Alumno> Alumno { get; set; }
         public DbSet<Materia> Materia { get; set; }
-        public DbSet<Profesor> Profesor { get; set; }
+        
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=VOSTRO\SQLEXPRESS2019;Initial catalog=Instituto2022;Integrated Security=true");
+            //optionsBuilder.UseSqlServer(@"Data Source=VOSTRO\SQLEXPRESS2019;Initial catalog=Instituto2022;Integrated Security=true");
+            optionsBuilder.UseSqlServer(@"Data Source=LOGAN\SQLEXPRESS;Initial catalog=Instituto2022;Integrated Security=true");
         }
     }
 
-    class Program
+    class Program : ProgramBase
     {
         static void Main(string[] args)
         {
@@ -69,236 +28,11 @@ namespace EFInstituto
             {
                 if (context.Maestro.Count() == 0 && context.Alumno.Count() == 0 && context.Materia.Count() == 0)
                 {
-                    CrearDatos(context);
+                    MenuCarga(context);
                 }
 
-                MostrarDatos(context);
-
-                Materia materia = new Materia();
-
-                #region Agregar dato
-                /*Console.Write("Descripción: ");
-                materia.Descripcion = Console.ReadLine();
-
-                Console.Write("Curso Division: ");
-                materia.CursoDivision = Console.ReadLine();
-
-                context.Materia.Add(materia); */
-                #endregion
-
-                #region Modificar dato
-                /*
-                Console.WriteLine("Datos a cambiar");
-                Console.Write("Id Nateria: ");
-                int id = int.Parse(Console.ReadLine());
-
-                materia = context.Materia.Where(x => x.id == id).FirstOrDefault();
-
-                Console.WriteLine("Descripción: {0}", materia.Descripcion);
-                Console.WriteLine("Curso Division: {0}", materia.CursoDivision);
-
-                Console.Write("Descripción: ");
-                materia.Descripcion = Console.ReadLine();
-
-                Console.Write("Curso Division: ");
-                materia.CursoDivision = Console.ReadLine();
-
-                context.SaveChanges();
-                */
-                #endregion
-
-                #region Borrar datos
-                /*
-                Console.WriteLine("Datos a borrar");
-                Console.Write("Id Nateria: ");
-                int id = int.Parse(Console.ReadLine());
-
-                materia = context.Materia.Where(x => x.id == id).FirstOrDefault();
-
-                context.Materia.Remove(materia);
-
-                context.SaveChanges();
-                */
-                #endregion
+                MainMenu(context);
             }
-        }
-
-        /// <summary>
-        /// Metodo para la creación de los la primera vez que se conecta con la DDBB.
-        /// </summary>
-        /// <param name="context">Una instancia de la clase DBContext</param>
-        public static void CrearDatos(EFInstitutoContext context)
-        {
-            #region Agrego Materias.
-            // Agrego las materias
-            Materia materia = new Materia();
-
-            materia.Descripcion = "Base de Datos Relacionales";
-            materia.CursoDivision = "1A";
-            context.Materia.Add(materia);
-
-            materia = new Materia();
-
-            materia.Descripcion = "Base de Datos No Relacionales";
-            materia.CursoDivision = "1A";
-            context.Materia.Add(materia);
-
-            materia = new Materia();
-
-            materia.Descripcion = "POO";
-            materia.CursoDivision = "2A";
-            context.Materia.Add(materia);
-
-            materia = new Materia();
-
-            materia.Descripcion = "Investigación Operativa";
-            materia.CursoDivision = "3A";
-            context.Materia.Add(materia);
-
-            materia = new Materia();
-
-            materia.Descripcion = "Ciencias de la Computación";
-            materia.CursoDivision = "2A";
-            context.Materia.Add(materia);
-
-            context.SaveChanges();
-            // Fin agregar Materias
-            #endregion
-
-            #region Agrego Maestros.
-            //Agrego los maestros
-            Maestro maestro = new Maestro();
-
-            maestro.Nombre = "Javier";
-            maestro.Apellido = "Chércoles";
-            maestro.MateriaId = 3;
-            context.Maestro.Add(maestro);
-
-            maestro = new Maestro();
-            maestro.Nombre = "José Luis";
-            maestro.Apellido = "Caero";
-            maestro.MateriaId = 1;
-            context.Maestro.Add(maestro);
-
-            maestro = new Maestro();
-            maestro.Nombre = "Juan Carlos";
-            maestro.Apellido = "Romero";
-            maestro.MateriaId = 5;
-            context.Maestro.Add(maestro);
-
-            maestro = new Maestro();
-            maestro.Nombre = "Mario";
-            maestro.Apellido = "Perello";
-            maestro.MateriaId = 2;
-            context.Maestro.Add(maestro);
-
-            maestro = new Maestro();
-            maestro.Nombre = "Guillermo";
-            maestro.Apellido = "Cherencio";
-            maestro.MateriaId = 4;
-            context.Maestro.Add(maestro);
-
-            context.SaveChanges();
-            //Fin agregar maestros
-            #endregion
-
-            #region Agrego Alumnos.
-            // Agregar los alumnos
-
-            Alumno alumno = new Alumno();
-
-            alumno.Nombre = "Matias";
-            alumno.Apellido = "Neto";
-            alumno.ProfesorId = 1;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Raul Andres";
-            alumno.Apellido = "Orlando";
-            alumno.ProfesorId = 1;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Leonardo Martin";
-            alumno.Apellido = "Alexandre";
-            alumno.ProfesorId = 2;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Gastón Israel";
-            alumno.Apellido = "Diaz";
-            alumno.ProfesorId = 2;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Lucas";
-            alumno.Apellido = "Carriquiry";
-            alumno.ProfesorId = 3;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Martin";
-            alumno.Apellido = "Carriquiry";
-            alumno.ProfesorId = 3;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Brandon Gaston";
-            alumno.Apellido = "Berrio";
-            alumno.ProfesorId = 4;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Monica";
-            alumno.Apellido = "Echegaray";
-            alumno.ProfesorId = 4;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Santiago";
-            alumno.Apellido = "Amenta";
-            alumno.ProfesorId = 5;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Thiago";
-            alumno.Apellido = "Miglioranza";
-            alumno.ProfesorId = 5;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Luciano Carlos";
-            alumno.Apellido = "Suppa";
-            alumno.ProfesorId = 1;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Pablo Ariel";
-            alumno.Apellido = "Soler";
-            alumno.ProfesorId = 1;
-            context.Alumno.Add(alumno);
-
-            alumno = new Alumno();
-
-            alumno.Nombre = "Alvaro";
-            alumno.Apellido = "Astellano";
-            alumno.ProfesorId = 2;
-            context.Alumno.Add(alumno);
-
-            context.SaveChanges();
-            // Fin agregar alumnos
-            #endregion
         }
 
         public static void MostrarDatos(EFInstitutoContext context)
